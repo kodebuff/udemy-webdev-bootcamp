83a76980908282ee27291fcf1c92a9c8436afba8
Leo Santillan <kodebuff@gmail.com>
2017-09-23 09:20:13 +0800

    modified while loop pset and added for loop pset

--- S09/S09-L108-for-loop-pset/script.js
+++ S09/S09-L108-for-loop-pset/script.js
@@ -1,5 +1,5 @@
 /***********************************************************
-USE WHILE LOOP TO:
+USE FOR LOOP TO:
 
 1. Print all numbers between -10 to 19
 
@@ -11,45 +11,32 @@ USE WHILE LOOP TO:
 
 ***********************************************************/
 
-var num;
-
 //1. Print all numbers between -10 to 19
 console.log("Print all numbers between -10 to 19");
-num = -10;
-while (num <= 19) {
-  console.log(num);
-  num++;
+for (var i = -10; i <= 19; i++) {
+  console.log(i);
 }
 
 // 2. Print all even numbers between 10 and 40
 console.log("Print all even numbers between 10 and 40");
-num = 10;
-while (num <= 40) {
-  //print if num is an even number
-  if (num % 2 === 0) {
-    console.log(num);
+for (var i = 10; i <= 40; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
   }
-  num++;
 }
 
 //3. Print all odd numbers between 300 and 333
 console.log("Print all odd numbers between 300 and 333");
-num = 300;
-while (num <= 333) {
-  //print if num is an odd number
-  if (num % 2 !== 0) {
-    console.log(num);
+for (var i = 300; i <= 333; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
   }
-  num++;
 }
 
 //4. Print all numbers divisible by 5 AND 3 between 5 and 50
 console.log("Print all numbers divisible by 5 AND 3 between 5 and 50");
-num = 5;
-while (num <= 50) {
-  //print if num is divisibla by 3 and 5
-  if ((num % 3 === 0) && (num % 5 === 0)) {
-    console.log(num);
+for (var i = 5; i <= 50; i++) {
+  if ((i % 3 === 0) && (i % 5 === 0)) {
+    console.log(i);
   }
-  num++;
 }
