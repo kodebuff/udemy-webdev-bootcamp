9dc385edbe27cf18d8df4043ceb572724d3aaea8
Leo Santillan <kodebuff@gmail.com>
2017-09-24 23:16:04 +0000

    updated array-pset content.

--- S11/S11-L130-array-pset/array-pset.js
+++ S11/S11-L130-array-pset/array-pset.js
@@ -1,2 +1,73 @@
-// alert("Array pset connected.");
+// 1. printReverse()
 
+function printReverse(arrs) {
+  for (var i = arrs.length - 1; i >= 0; i--) {
+    console.log(arrs[i]);
+  }
+}
+
+
+// 2. isUniform()
+
+// //VERSION 1
+// function isUniform(arrs) {
+//   //store first array to a variable
+//   var compareElement = arrs[0];
+  
+//   for (var i = 1; i < arrs.length; i++) {
+//     if (arrs[i] === compareElement) {
+//       compareElement = arrs[i];
+//       if (i === (arrs.length - 1)) {
+//         return true;
+//       }
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+
+// VERSION 2
+function isUniform(arrs) {
+  //store first array to a variable
+  var compareElement = arrs[0];
+  for (var i = 1; i < arrs.length; i++) {
+    if (arrs[i] !== compareElement) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+// 3. sumArray()
+
+// My Sol
+// function sumArray(arrs) {
+//   var result = 0;
+//   for (var i = 0; i < arrs.length; i++) {
+//     result += arrs[i];
+//   }
+//   return result;
+// }
+
+// Colt's solution
+function sumArray(arrs) {
+  var total = 0;
+  arrs.forEach(function(arr) {
+    total += arr;
+  });
+  return total;
+}
+
+
+// 4. max()
+
+function max(arrs) {
+  var maxNum = arrs[0];
+  for (var i = 1; i < arrs.length; i++) {
+    if (arrs[i] > maxNum) {
+      maxNum = arrs[i];
+    }
+  }
+  return maxNum;
+}
