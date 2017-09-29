1e32df82816a8f26aa8466b916682fc374cac901
Leo Santillan <kodebuff@gmail.com>
2017-09-29 09:00:06 +0000

    modified S18 to do list app

--- S18/TodoListProject/assets/js/todos.js
+++ S18/TodoListProject/assets/js/todos.js
@@ -1,11 +1,24 @@
+// if we have something on local storage place that
+if(localStorage.getItem('todos')) {
+  $('#todos').html(localStorage.getItem('todos'));
+}
+
 $("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
+  
+  //saving data to html local storage
+  var todos = $("#todos").html();
+  localStorage.setItem('todos', todos);
 });
 
 //click on X to delete a Todo
 $("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
+    
+    //saving data to html local storage
+    var todos = $("#todos").html();
+    localStorage.setItem('todos', todos);
   });
   event.stopPropagation(); 
 });
@@ -17,9 +30,12 @@ $("input[type='text']").on("keypress", function(event){
     //clear the text after hitting enter
     $(this).val("");
     //create new li and add todo to ul
-    $("ul").append("<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span> " + todoText + "</li>");
+    //$("ul").append("<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span> " + todoText + "</li>");
+    $("ul").prepend("<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span> " + todoText + "</li>");
     
-    //console.log($(this).val());
+    //saving data to html local storage
+    var todos = $("#todos").html();
+    localStorage.setItem('todos', todos);
   }
 });
 
